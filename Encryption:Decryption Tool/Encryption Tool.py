6b9e2413bc746794bfcd79381526430f763587e3
ishaan2252 <97852294+ishaan2252@users.noreply.github.com>
2024-03-12 13:41:52 -0700

    Update Encryption Tool.py

--- Encryption:Decryption Tool/Encryption Tool.py
+++ Encryption:Decryption Tool/Encryption Tool.py
@@ -3,4 +3,22 @@
 
 import cryptography as crypt
 
-help(crypt)
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key()
+f = Fernet(key)
+# Store this key somewhere safe :) 
+print(f"This is your key: {key}")
+
+user_messagetoencrypt = input("Enter your Message to Encrypt Here")
+
+token = crypt.encrypt(user_messagetoencrypt)
+
+print("This is your encrypted data:")
+print(token) 
+
+
+originaldata = f.decrypt(token)
+
+print("This is the original data:")
+print(originaldata)
