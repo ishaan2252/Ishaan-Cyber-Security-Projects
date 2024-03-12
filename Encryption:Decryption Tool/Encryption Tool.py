769789abdb5187333fc2231e3e3a35872da53d50
ishaan2252 <97852294+ishaan2252@users.noreply.github.com>
2024-03-12 12:35:22 -0700

    Create Encryption Tool.py

---
+++ Encryption:Decryption Tool/Encryption Tool.py
@@ -0,0 +1,6 @@
+# This piece of code will encrypt and decrypt messages using a simple cipher like Caesar Cipher or more advanced algos like AES. This will focus on user input, error handling and clear output.
+# Skills practiced will be cryptography concepts, string manipulation, and file I/O
+
+import cryptography as crypt
+
+help(crypt)
