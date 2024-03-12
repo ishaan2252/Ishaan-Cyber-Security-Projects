f894a9945b6fff2fcca0f5d2d1f22c79ef20f586
ishaan2252 <97852294+ishaan2252@users.noreply.github.com>
2024-03-12 14:12:27 -0700

    Update Encryption Tool.py

--- Encryption:Decryption Tool/Encryption Tool.py
+++ Encryption:Decryption Tool/Encryption Tool.py
@@ -5,15 +5,19 @@ import cryptography
 
 from cryptography.fernet import Fernet
 
+# Generating Cryptographic Key, printing this out to the end user. Would not do this "typically", but doing this for ease of readability to user
 key = Fernet.generate_key()
 f = Fernet(key)
 # Store this key somewhere safe :) 
-print(f"This is your key: {str(key)}")
+print(f"This is your key, store this in a safe place: {str(key)}")
 
+# Prompt the end user to input the string they want encrypted.
 user_messagetoencrypt = input("Enter your Message to Encrypt Here: ")
 
+# Encrypt the message entered using the token stored as "f"
 token = f.encrypt(user_messagetoencrypt.encode())
 
+# Print to user the token (encrypted data)
 print("This is your encrypted data:")
 print(token) 
 
