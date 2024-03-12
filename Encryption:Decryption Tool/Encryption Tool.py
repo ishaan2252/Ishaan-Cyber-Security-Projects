6260bf055d017199a3245e2a8cb3be2f3de3a169
ishaan2252 <97852294+ishaan2252@users.noreply.github.com>
2024-03-12 14:10:00 -0700

    Update Encryption Tool.py

--- Encryption:Decryption Tool/Encryption Tool.py
+++ Encryption:Decryption Tool/Encryption Tool.py
@@ -1,18 +1,18 @@
 # This piece of code will encrypt and decrypt messages using a simple cipher like Caesar Cipher or more advanced algos like AES. This will focus on user input, error handling and clear output.
 # Skills practiced will be cryptography concepts, string manipulation, and file I/O
 
-import cryptography as crypt
+import cryptography
 
 from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
 f = Fernet(key)
 # Store this key somewhere safe :) 
-print(f"This is your key: {key}")
+print(f"This is your key: {str(key)}")
 
-user_messagetoencrypt = input("Enter your Message to Encrypt Here")
+user_messagetoencrypt = input("Enter your Message to Encrypt Here: ")
 
-token = crypt.encrypt(user_messagetoencrypt)
+token = f.encrypt(user_messagetoencrypt.encode())
 
 print("This is your encrypted data:")
 print(token) 
@@ -21,4 +21,4 @@ print(token)
 originaldata = f.decrypt(token)
 
 print("This is the original data:")
-print(originaldata)
+print(originaldata.decode())
